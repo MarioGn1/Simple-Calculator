3b2aa3d810958ed9e6a5ca80f8f0823decb2fd50
Mario Matsov <mariogn@mail.bg>
2023-03-25 16:21:13 +0200

    Create Generic Service Result

---
+++ src/server/Calculator/Services/Models/ServiceResult.cs
@@ -0,0 +1,31 @@
+﻿namespace Calculator.Services.Models;
+
+public sealed class ServiceResult<T> where T : struct
+{
+    private readonly List<string> _errorMessages;
+
+    public ServiceResult()
+    {
+        _errorMessages = new List<string>();
+    }
+
+    public T Data { get; private set; }
+
+    public bool Success => _errorMessages.Count == 0;
+
+    public Task SetResult(T result)
+    {
+        Data = result;
+
+        return Task.CompletedTask;
+    }
+
+    public Task AddMessage(string message)
+    {
+        _errorMessages.Add(message);
+
+        return Task.CompletedTask;
+    }
+
+    public IReadOnlyCollection<string> ErrorMessages => _errorMessages;
+}
