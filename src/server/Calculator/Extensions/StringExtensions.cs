b9bab442e2b53bec54b5c711e1f3badd4e4bf52e
Mario Matsov <mariogn@mail.bg>
2023-03-26 18:44:17 +0300

    Create StringExtensions.cs

---
+++ src/server/Calculator/Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Calculator.Extensions;
+
+public static class StringExtensions
+{
+    public static string RemoveOutherParentheses(this string expression)
+    {
+        var startParenthesis = expression.IndexOf('(');
+        var endParenthesis = expression.LastIndexOf(')');
+
+        return expression.Remove(startParenthesis, 1).Remove(endParenthesis - 1);
+    }
+}
