0815ffe646da97f10ee33832e9ad3f7744f9e529
Mario Matsov <mariogn@mail.bg>
2023-03-26 22:20:25 +0300

    Create Test Data

---
+++ src/server/Calculator.IntegrationTests/TestData/EndpointInvalidTestData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+
+namespace Calculator.IntegrationTests.TestData;
+
+internal class EndpointInvalidTestData : IEnumerable<object[]>
+{
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        yield return new object[] { ""};
+        yield return new object[] { "-(-5+2*1"};
+        yield return new object[] { "-(-5+2*1))"};
+        yield return new object[] { "-5+2*1)"};
+        yield return new object[] { "(- (1 + 2) * 4) / 6)"};
+        yield return new object[] { "- a * (4 + 5) "};
+        yield return new object[] { "- 2 + 3 +(4 + 5) = 6"};
+        yield return new object[] { "- 2 + 3 +(4_4 + 5) * 6"};
+        yield return new object[] { "- 2 + 3 +(4^4 + 5) * 6"};
+        yield return new object[] { "- 2 + 3 +(4%4 + 5) * 6"};
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
---
+++ src/server/Calculator.IntegrationTests/TestData/EndpointValidTestData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+
+namespace Calculator.IntegrationTests.TestData;
+
+internal class EndpointValidTestData : IEnumerable<object[]>
+{
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        yield return new object[] { "-(-5+2*1)", 3 };
+        yield return new object[] { "-(-5+2*1) - (-5+2*1)", 6 };
+        yield return new object[] { "-(-5+2*1) + (-5+2*1)", 0 };
+        yield return new object[] { "- (1 + 2) * 4 / 6", -2 };
+        yield return new object[] { "- 3 * (4 + 5) ", -27 };
+        yield return new object[] { "- 2 + 3 +(4 + 5) * 6", 55 };
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
