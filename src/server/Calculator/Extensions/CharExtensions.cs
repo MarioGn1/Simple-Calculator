e17089113a75a946d7ed36f7519d87f21dac8986
Mario Matsov <mariogn@mail.bg>
2023-03-26 18:43:34 +0300

    Create CharExtensions.cs

---
+++ src/server/Calculator/Extensions/CharExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Calculator.Extensions;
+
+public static class CharExtensions
+{
+    public static double Run(this char op, double left, double right)
+    {
+        switch (op)
+        {
+            case '+':
+                return left + right;
+            case '-':
+                return left - right;
+            case '/':
+                return left / right;
+            case '*':
+                return left * right;
+        }
+        throw new NotSupportedException();
+    }
+}
