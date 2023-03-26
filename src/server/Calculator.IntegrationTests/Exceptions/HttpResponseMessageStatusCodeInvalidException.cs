09ba74667921cbb6a622a09c804a0da42a6ca45e
Mario Matsov <mariogn@mail.bg>
2023-03-26 22:20:04 +0300

    Create HttpResponseMessageStatusCodeInvalidException.cs

---
+++ src/server/Calculator.IntegrationTests/Exceptions/HttpResponseMessageStatusCodeInvalidException.cs
@@ -0,0 +1,16 @@
+﻿namespace Calculator.IntegrationTests.Exceptions;
+
+public sealed class HttpResponseMessageStatusCodeInvalidException : InvalidOperationException
+{
+    public HttpResponseMessageStatusCodeInvalidException(HttpStatusCode? expectedStatusCode, HttpStatusCode receivedStatusCode, string? receivedContent)
+        : base($"Expected status code '{expectedStatusCode?.ToString() ?? "not provided"}' but was '{receivedStatusCode}'. Received content = '{receivedContent}'")
+    {
+        ExpectedStatusCode = expectedStatusCode;
+        ReceivedStatusCode = receivedStatusCode;
+        ReceivedContent = receivedContent;
+    }
+
+    public HttpStatusCode? ExpectedStatusCode { get; }
+    public HttpStatusCode ReceivedStatusCode { get; }
+    public string? ReceivedContent { get; }
+}
