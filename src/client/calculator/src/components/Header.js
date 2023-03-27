5761bdcef82508e00a2c2480334d43f12416b612
Mario Matsov <mariogn@mail.bg>
2023-03-27 16:17:04 +0300

    Create Header.js

---
+++ src/client/calculator/src/components/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+
+    return (
+        <header id="site-header">
+            <nav className="navbar">
+                <section className="navbar-dashboard">
+                    <div>Calculator</div>
+                </section>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
